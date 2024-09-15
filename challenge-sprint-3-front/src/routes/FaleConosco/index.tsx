7cb60829b06ae5d61f23d5e23527671ae90b2bc3
GuiRomanholi <guilhermeroma343@gmail.com>
2024-09-15 18:50:16 -0300

    estilizando fale conosco

--- challenge-sprint-3-front/src/routes/FaleConosco/index.tsx
+++ challenge-sprint-3-front/src/routes/FaleConosco/index.tsx
@@ -1,4 +1,4 @@
-
+import styles from '../../style/Fale.module.css';
 import fale_conosco from '../../../public/img/fotoFaleConosco.png';
 import icone_email from '../../../public/img/icone_email_azul.png';
 import icone_whats from '../../../public/img/icone_whats_azul.png';
@@ -8,34 +8,34 @@ export default function Fale(){
 
     return(
         <div>
-            <section>
-                <div>
+            <section className={styles.section1}>
+                <div className={styles.headline}>
                     <h1>Fale conosco!</h1>
                     <p>Estamos sempre prontos para ouvir você! Se tiver <strong> dúvidas, sugestões ou críticas, </strong> não hesite em entrar em contato. Nossa equipe retornará o mais breve possível.</p>
                 </div>
-                <div>
+                <div className={styles.imgHeadline}>
                     <img src={fale_conosco} alt="fale_conosco"/>
                 </div>
             </section>
 
-            <section>
-                <div>
+            <section className={styles.section2}>
+                <div className={styles.email}>
                     <img src={icone_email} alt="icone_email"/>
-                    <div>
+                    <div className={styles.textEmail}>
                         <h2>Email</h2>
                         <p>consertcars@gmail.com</p>
                     </div>
                 </div>
-                <div>
+                <div className={styles.whats}>
                     <img src={icone_whats} alt="icone_whats"/>
-                    <div>
+                    <div className={styles.textWhats}>
                         <h2>Telefone</h2>
                         <p>(11)9 9999 9999</p>
                     </div>
                 </div>
             </section>
 
-            <article>
+            <article className={styles.article}>
                 <h2>Tem alguma dúvida sobre o Consert Car's?</h2>
                 <p>Já deu uma olhada nas Dúvidas Frequentes? Pode ser que a sua resposta já esteja por lá!
                     Ainda quer falar com a gente? Entre em contato pelo e-mail ou contact-nos por telefone.
