47940c09eeb1598ff0ce6d881de0ca3eb4f8201a
GuiRomanholi <guilhermeroma343@gmail.com>
2024-09-15 17:17:03 -0300

    fazendo fale conosco e sobre nos

--- challenge-sprint-3-front/src/routes/FaleConosco/index.tsx
+++ challenge-sprint-3-front/src/routes/FaleConosco/index.tsx
@@ -1,14 +1,45 @@
-//import { useState } from "react";
-//import { useNavigate, useParams } from "react-router-dom";
 
+import fale_conosco from '../../../public/img/fotoFaleConosco.png'
+import icone_email from '../../../public/img/icone_email_azul.png'
+import icone_whats from '../../../public/img/icone_whats_azul.png'
 
-//Comentei por que fica vermelho sem nenhum conteudo mas tem que importar mesmo
 
 export default function Fale(){
 
     return(
         <div>
-            <h1>Fale Conosco</h1>
+            <section>
+                <div>
+                    <h1>Fale conosco!</h1>
+                    <p>Estamos sempre prontos para ouvir você! Se tiver <strong> dúvidas, sugestões ou críticas, </strong> não hesite em entrar em contato. Nossa equipe retornará o mais breve possível.</p>
+                </div>
+                <div>
+                    <img src={fale_conosco} alt="fale_conosco"/>
+                </div>
+            </section>
+
+            <section>
+                <div>
+                    <img src={icone_email} alt="icone_email"/>
+                    <div>
+                        <h2>Email</h2>
+                        <p>consertcars@gmail.com</p>
+                    </div>
+                </div>
+                <div>
+                    <img src={icone_whats} alt="icone_whats"/>
+                    <div>
+                        <h2>Telefone</h2>
+                        <p>(11)9 9999 9999</p>
+                    </div>
+                </div>
+            </section>
+
+            <article>
+                <h2>Tem alguma dúvida sobre o Consert Car's?</h2>
+                <p>Já deu uma olhada nas Dúvidas Frequentes? Pode ser que a sua resposta já esteja por lá!
+                    Ainda quer falar com a gente? Entre em contato pelo e-mail ou contact-nos por telefone.</p>
+            </article>
         </div>
     );
 }
--- challenge-sprint-3-front/src/routes/SobreNos/index.tsx
+++ challenge-sprint-3-front/src/routes/SobreNos/index.tsx
@@ -1,14 +1,47 @@
-//import { useState } from "react";
-//import { useNavigate, useParams } from "react-router-dom";
 
-//Comentei por que fica vermelho sem nenhum conteudo mas tem que importar mesmo
+import sobre_nos from '../../../public/img/fotoSobreNos02.png'
+import cristian_foto from '../../../public/img/foto_cristan.png'
+import guilherme_foto from '../../../public/img/foto_guilherme.png'
+import igor_foto from '../../../public/img/foto_igor.png'
+
 
 
 export default function Sobre(){
 
     return(
         <div>
-            <h1>Sobre Nós</h1>
+            <section>
+                <img src={sobre_nos} alt="sobre_nos"/>
+            </section>
+            <article>
+                <div>
+                    <h1>Sobre nós</h1>
+                    <p>Somos estudantes da sala 1TDSPM da FIAP e estamos desenvolvendo um projeto inovador que utiliza tecnologia de ponta para simplificar e aprimorar a maneira como os usuários lidam com problemas em seus veículos e solicitam serviços de guincho, oferecendo conveniência, rapidez e transparência, ou seja,  uma experiência completa e satisfatória para pessoas com problemas em seus veículos e resolvem utilizar nossa aplicação. </p>
+                </div>
+            </article>
+            <section>
+                <h2>Nosso Time</h2>
+                <div>
+                    <div>
+                        <img src={cristian_foto} alt="cristian_foto"/>
+                        <h3>Cristian Caja</h3>
+                        <p>RM: 558502</p>
+                        <p><a href="https://github.com/cristianrcaja" target="_blank" >GitHub</a></p>
+                    </div>
+                    <div>
+                        <img src={guilherme_foto} alt="guilherme_foto"/>
+                        <h3>Guilherme Romanholi</h3>
+                        <p>RM: 557462</p>
+                        <p><a href="https://github.com/GuiRomanholi" target="_blank">GitHub</a></p>
+                    </div>
+                    <div>
+                        <img src={igor_foto} alt="igor_foto"/>
+                        <h3>Igor Werneck</h3>
+                        <p>RM: 558395</p>
+                        <p><a href="https://github.com/IgorWJ" target="_blank" >GitHub</a></p>
+                    </div>
+                </div>
+            </section>
         </div>
     );
 }
