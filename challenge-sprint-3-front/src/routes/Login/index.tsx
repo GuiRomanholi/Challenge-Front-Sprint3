0cf7f5016561cc226d2cfdfb192b307d85fc452e
GuiRomanholi <guilhermeroma343@gmail.com>
2024-09-16 15:05:30 -0300

    Login feito

--- challenge-sprint-3-front/src/routes/Login/index.tsx
+++ challenge-sprint-3-front/src/routes/Login/index.tsx
@@ -1,32 +1,51 @@
 
-
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import styles from '../../style/Login.module.css';
 
 export default function Login() {
+    const navigate = useNavigate();
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        // Após o submit, apenas definimos isLoggedIn como true
+        setIsLoggedIn(true);
+
+        setTimeout(() => {
+            navigate('/'); // Certifique-se de que "/home" seja o caminho correto para sua página Home
+        }, 2000);
+    };
+
     return (
         <div className={styles.loginContainer}>
-            <form id="form-login" className={styles.loginForm} action="../pag-chamado/chamado.html" method="get">
-                <h2>Login</h2>
-                <input
-                    type="text"
-                    name="txtLogin"
-                    id="idLogin"
-                    className={styles.inputField}
-                    placeholder="Usuário"
-                    required
-                />
-                <input
-                    type="password"
-                    name="txtSenha"
-                    id="idSenha"
-                    className={styles.inputField}
-                    placeholder="Senha"
-                    required
-                />
-                <button type="submit" className={styles.submitButton}>Entrar</button>
-                <Link className={styles.createAccountLink} to={"/cadastro"}>Criar uma conta</Link>
-            </form>
+            {isLoggedIn ? (
+                <div className={styles.successMessage}>
+                    Você está logado!
+                </div>
+            ) : (
+                <form id="form-login" className={styles.loginForm} onSubmit={handleSubmit}>
+                    <h2>Login</h2>
+                    <input
+                        type="text"
+                        name="txtLogin"
+                        id="idLogin"
+                        className={styles.inputField}
+                        placeholder="Usuário"
+                        required
+                    />
+                    <input
+                        type="password"
+                        name="txtSenha"
+                        id="idSenha"
+                        className={styles.inputField}
+                        placeholder="Senha"
+                        required
+                    />
+                    <button type="submit" className={styles.submitButton}>Entrar</button>
+                    <Link className={styles.createAccountLink} to={"/cadastro"}>Criar uma conta</Link>
+                </form>
+            )}
         </div>
     );
 }
